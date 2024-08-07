c1cf5d845c150fa31a60d98a3cd25dc3904f206d
Marc Marimon <crammarimon@hotmail.com>
2024-08-07 10:39:10 +0200

    Agregar archivos de proyecto.

---
+++ LoginMVC/Controllers/AuthenticationController.cs
@@ -0,0 +1,43 @@
+﻿using LoginMVC.DAL;
+using LoginMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginMVC.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        private DALLogin dal = new DALLogin();
+
+        // GET: Authentication
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        // POST: Authentication
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel vm)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (dal.IsValidUser(vm.Email, vm.Password))
+                {
+                    return RedirectToAction("Index", "Home");
+                   
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Nombre o contraseña incorrectos.";
+                }
+            }
+            
+            return View(vm);
+        }
+    }
+}
---
+++ LoginMVC/DAL/DALLogin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace LoginMVC.DAL
+{
+    public class DALLogin
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public bool IsValidUser(string email, string password)
+        {
+            bool isValid = false;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT TOP(1) * FROM Credentials WHERE Email = @Email AND Password = @Password";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.Parameters.AddWithValue("@Password", password);
+
+                    isValid = cmd.ExecuteScalar() != null;
+                }
+                conn.Close();
+            }
+
+            return isValid;
+        }
+    }
+}
